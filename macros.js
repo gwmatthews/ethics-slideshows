7f301245b5510eb33361ce015db702c9c15d3579
yurek-0 <geoslack@gmail.com>
2019-11-26 21:26:15 -0500

    renamed images folder fixed css

---
+++ macros.js
@@ -0,0 +1,4 @@
+remark.macros.scale = function (percentage, frameColor) {
+  var url = this;
+  return '<img class="photo" src="' + url + '" style="width: ' + percentage + ';border: 7px solid ' + frameColor + ';    " />';
+};
