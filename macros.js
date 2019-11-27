d929e57feb82795dda838d05962025e5db59fbb0
yurek-0 <geoslack@gmail.com>
2019-11-27 13:38:35 -0500

    fixed style added slideshow 2

--- macros.js
+++ macros.js
@@ -2,3 +2,5 @@ remark.macros.scale = function (percentage, frameColor) {
   var url = this;
   return '<img class="photo" src="' + url + '" style="width: ' + percentage + ';border: 7px solid ' + frameColor + ';    " />';
 };
+
+
