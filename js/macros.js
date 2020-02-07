12e76659b7bc7677bd4f96d029b0431dce79a5ce
gwmatthews <geoslack@gmail.com>
2020-02-07 11:10:30 -0500

    adjust layout, redo css files, add macros

--- js/macros.js
+++ js/macros.js
@@ -8,3 +8,13 @@ remark.macros.jump = function (linkText) {
   var url = this;
   return '<a href="' + url + '"target="_blank">' + linkText + '</a>';
 }
+
+remark.macros.portrait = function (person, dates, percentage) {
+  var url = this;
+  return '<div class="center"> <img class="photo" src="' +  url  + '" style="width:' + percentage  + '; border: 4px solid gray;" ><span class="bottomcap">' + person + ' <br> ' + dates +  '</span></div>' ;
+  
+};
+
+remark.macros.vspace = function (size) {
+  return '<div style="height: ' + size + 'px;"><br></div>';
+};
